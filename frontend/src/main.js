9e01f36a805e7110374dcf98c01e47f1c69be633
이준영 <8byte@miridih.com>
2021-01-14 00:38:36 +0900

    fe-01 routes added it

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Home from './components/Home'
+import About from './components/About'
+import NotFound from "@/components/NotFound"
 
 Vue.config.productionTip = false
 
+const routes = {
+  '/': Home,
+  '/about': About
+}
+
 new Vue({
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent() {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
   render: h => h(App),
 }).$mount('#app')
