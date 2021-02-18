0e669acf2635bb82e4bd760041274fdf2e0ec1e0
64byte <8byte@naver.com>
2021-02-18 13:01:32 +0900

    Feature/fe 04 (#7)
    * added to vue router
    
    * added application.yml
    
    * updated vuetify redesigned main
    
    * updated vuetify redesigned main
    
    * added dockerfile

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,46 +1,67 @@
 <template>
-  <div id="app">
-    <div class="router-links">
-      <router-link class="nav-links" to="/">Home</router-link>
-      <router-link class="nav-links" to="/article">Article</router-link>
-    </div>
-    <router-view></router-view>
-  </div>
+  <v-app>
+    <v-navigation-drawer app
+    class="align-center">
+      <v-row>
+        <router-link class="nav-links" to="/">
+          <v-icon
+              color="black">
+            Home
+          </v-icon>
+        </router-link>
+      </v-row>
+      <v-row>
+        <router-link class="nav-links" to="/article">
+          <a style="color: black;">
+            Article
+          </a>
+        </router-link>
+      </v-row>
+      <v-row>
+        <router-link class="nav-links" to="/about">
+          <a style="color: black">
+            About
+          </a>
+        </router-link>
+      </v-row>
+    </v-navigation-drawer>
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <v-footer
+        color="black"
+        padless
+        app>
+      <v-row
+          justify="center"
+          no-gutters
+      >
+        <div
+            color="white">
+          test
+        </div>
+      </v-row>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App',
 }
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-
-  .router-link-active {
-    color: white;
-    background-color: red;
-  }
-
-  .router-link-exact-active {
-    color: white;
-    background-color: blue;
-  }
-
-  .router-links {
-
-  }
-
   .nav-links {
-    margin: 10px;
-    display: inline-block;
+    margin: 30px;
+    color: black;
+    font-family: "Droid Serif";
   }
 </style>
